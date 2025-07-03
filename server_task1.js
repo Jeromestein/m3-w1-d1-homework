b61beda996f2897282043f76adb9e5b7e9ef33b4
Jerome <nietsemorej@gmail.com>
2025-07-03 13:43:49 -0700

    Add basic HTTP server functionality to server_task1.js

---
+++ server_task1.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('Home Page.');
+  } else if (req.url === '/about') {
+    res.statusCode = 200;
+    res.end('About Page.');
+  } else if (req.url === '/contact') {
+    res.statusCode = 200;
+    res.end('Contact Page.');
+  } else {
+    res.statusCode = 404;
+    res.end('Invalid Request!');
+  }
+});
+
+// Start the server and listen on port 5000
+server.listen(4999, () => {
+  console.log('The NodeJS server on port 5000 is now running….');
+});
